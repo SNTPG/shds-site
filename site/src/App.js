747e3818aebd61f898cb069f98a419db257d60a3
SNTPG <sntpg@ucla.edu>
2022-12-27 18:19:22 -0800

    Changing favicon

--- site/src/App.js
+++ site/src/App.js
@@ -1,7 +1,13 @@
+import React from 'react';
 import './App.css';
 import Tablet from './pages/Tablet.js';
+import {useEffect} from 'react';
 
 function App() {
+  useEffect(() => {
+      document.title = "Solitaire Hope Dream Simulator";
+    }
+  )
   return (
       <header className="App-header">
         <Tablet/>
