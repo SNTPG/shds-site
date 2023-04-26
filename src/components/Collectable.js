06557ad7992053dbae65f52690d4beb0e887bdf8
SNTPG <sntpg@ucla.edu>
2023-04-26 15:02:31 -0700

    Chapter 2 Motive Day 1

--- src/components/Collectable.js
+++ src/components/Collectable.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+const aliases = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "-1", "↋0"];
+
 function Collectable(props){
-    const indices = Array.from(Array(16).keys());
+    const indices = Array.from(Array(24).keys());
     const gacha = indices.map((num) =>
-        <div style={{"grid-area": `${"c"+String(num).padStart(2, '0')}`}} key={num} className={"collectableItem "+( (Number.isInteger(props.collectables.find((x => x === num)))) ? "found " : "notFound ") + ( (Number.isInteger(props.actualCollectables.find((x => x === num)))) ? "possessed" : "notPossessed")} id={"collectable"+String(num).padStart(2, '0')} >
-            <p>{String(num).padStart(2, '0')}</p>
+        <div style={{"grid-area": `${"c"+String(num).padStart(2, '0')}`}} key={num} className={"collectableItem "+( (Number.isInteger(props.collectables.find((x => x === num)))) ? "found " : "notFound ") + ( (Number.isInteger(props.actualCollectables.find((x => x === num)))) ? "possessed" : "notPossessed")} id={"collectable"+aliases[num]} >
+            <p>{aliases[num]}</p>
         </div>
     );
     
